afcdf7a20c8245e94f5ef30b30291ef269169568
Alexandre Díaz <dev@redneboa.es>
2016-12-13 19:19:51 +0100

    [CLEAN] Removed unused javascript and css sources... and better folders structure

--- aloxa_eiqui/static/src/js/website.js
+++ aloxa_eiqui/static/src/js/website.js
@@ -1,4 +1,22 @@
-/* Copyright (C) 2016 Solucións Aloxa S.L. <info@aloxa.eu> */
+/*#################################################################################
+#
+#    Copyright (C) 2016 Solucións Aloxa S.L. <info@aloxa.eu>
+#						Alexandre Díaz <alex@aloxa.eu>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################*/
 odoo.define('aloxa_eiqui.website', function (require) {
 	'use strict';
 	var ajax = require('web.ajax');
@@ -12,10 +30,10 @@ odoo.define('aloxa_eiqui.website', function (require) {
 		
 		////////////////////////
 		onLoad: function() {
-			$(function(){AloxaEiqui.onPageLoad();});
+			jQuery(function($){AloxaEiqui.onPageLoad($);});
 		},
 		
-		onPageLoad: function(ev) {
+		onPageLoad: function($) {
 			$('.select2-control').each(function(){ $(this).select2(); });
 			
 			$('.eiqui-message-item').each(function(i, elm){
