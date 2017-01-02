57176e0b0bb23462e60e9818db25eac943e81dcd
Alexandre Díaz <dev@redneboa.es>
2017-01-02 18:28:54 +0100

    [FIX] Plan creation

--- aloxa_eiqui/controllers/website.py
+++ aloxa_eiqui/controllers/website.py
@@ -347,8 +347,9 @@ class EiquiWebsite(webmain.Home):
         # forzamos que se guarden los cambios para asegurarnos de que el hilo pueda ver el nuevo proyecto
         request.cr.commit()
         kwargs = {'uid': request.uid, 'db': request.db, 'project_id': proj_id.id}
-        p = Process(target=self._thread_create_docker, args=(kwargs,))
-        p.start()
+        #p = Process(target=self._thread_create_docker, args=(kwargs,))
+        #p.start()
+        self._thread_create_docker(kwargs)
         return { 'check': True }
         
     def _thread_create_docker(self, kwargs):
