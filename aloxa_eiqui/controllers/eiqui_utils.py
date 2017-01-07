9553e8763f0a9eb56cff89d48b98e9d873ab46a6
Alexandre Díaz <dev@redneboa.es>
2017-01-07 19:22:16 +0100

    [FIX] ...

--- aloxa_eiqui/controllers/eiqui_utils.py
+++ aloxa_eiqui/controllers/eiqui_utils.py
@@ -124,36 +124,16 @@ def get_client_recipe_info(client, is_test=False):
 # RETURN
 #    Tupla: El código de salida del script, lo que imprimió por stdout y lo que imprimió por stderr
 #
-def call_test_dockermachine(params):
-    _logger.info("CALL SCRIPT 1")
-    eiquiscript = "sudo /usr/local/bin/docker-machine ip %s" % (' '.join(params))
-    _logger.info("CALL SCRIPT 2")
-    proc = Popen(eiquiscript, shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE, cwd=CWD)
-    _logger.info("CALL SCRIPT 3")
-    (out, err) = proc.communicate()
-    _logger.info("CALL SCRIPT 4")
-    return (proc.returncode, out, err)
-
 def call_eiqui_script(script, params):
     _logger.info("CALL SCRIPT 1")
     eiquiscript = "/usr/bin/sudo %s/ei_%s %s" % (CWD, script, ' '.join(params))
     _logger.info("CALL SCRIPT 2")
-    proc = Popen(eiquiscript, shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE, cwd=CWD, close_fds=True, preexec_fn=os.setsid)
+    proc = Popen(eiquiscript, shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE, cwd=CWD, 
+                 close_fds=True, preexec_fn=os.setsid)
     _logger.info("CALL SCRIPT 3")
     (out, err) = proc.communicate()
     _logger.info("CALL SCRIPT 4")
     return (proc.returncode, out, err)
-
-def create_dock_mach(client):
-    if not re.match(EIQUI_CLIENTNAME_REGEX, client):
-        raise Exception('Invalid Client Name!')
-    _logger.info("CREATE DOCK MACH 1")
-    (rcode, out, err) = call_test_dockermachine([client])
-    _logger.info("CREATE DOCK MACH 2")
-    if rcode == 0:
-        return True
-    _logger.info("CREATE DOCK MACH 3")
-    raise Exception('Return Code: %d\nOut: %s\nErr: %s\n' % (rcode,out,err))
 
 def create_client(client, branch="9.0"):
     if not re.match(EIQUI_CLIENTNAME_REGEX, client):
