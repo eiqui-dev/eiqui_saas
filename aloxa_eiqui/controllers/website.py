f64085ba5bc1c7b7f81bdc36fb0e5517b6052437
Alexandre Díaz <dev@redneboa.es>
2017-02-14 17:31:30 +0100

    [FIX] Docker creation from panel

--- aloxa_eiqui/controllers/website.py
+++ aloxa_eiqui/controllers/website.py
@@ -369,8 +369,8 @@ class EiquiWebsite(webmain.Home):
                     if not project:
                         raise Exception(_("The project appears doesn't exists!"))
                     # Crear cliente
-                    eiqui_utils.create_client(project.name, 
-                                              branch=project.odoo_version)
+                    #eiqui_utils.create_client(project.name, 
+                    #                          branch=project.odoo_version)
                     # Preparar Odoo (Produccion)
                     eiqui_config = env['eiqui.config.settings'].search([], order="id DESC", limit=1)
                     git_username = None
@@ -391,6 +391,10 @@ class EiquiWebsite(webmain.Home):
                     branch_repos = project.repo_modules_ids.search([('repo_id.branch', '=', project.odoo_version)])
                     for repo in branch_repos:
                         repos.append(repo.url)
+                        
+                    _logger.info('PASA POR AKI!')
+                    _logger.info(repos)
+                    _logger.info(modules)
                     
                     (inst_info, adminpasswd, odoo_url) = eiqui_utils.prepare_client_instance(
                         project.name,
